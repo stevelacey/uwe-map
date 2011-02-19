a018ca2833ff0236a8f27a48436af7f49d9938bc
Steve Lacey <steve@stevelacey.net>
2011-02-19 21:31:53 +0000

    Added 'Apple extension' tags for app icon, startup image and hidden Safari UI.

--- layout/header.php
+++ layout/header.php
@@ -2,6 +2,9 @@
 <html>
 	<head>
 	<title>Interaction Design Map App</title>
+  <meta name="apple-mobile-web-app-capable" content="yes"/>
+  <link rel="apple-touch-icon" href="/images/apple-touch-icon.png"/>
+  <link rel="apple-touch-startup-image" href="/images/startup.png">
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.css"/>
 	<link rel="stylesheet" href="/css/main.css"/>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
