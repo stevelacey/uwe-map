8553ab53f5695a22ed490e73391bad1a13b6248d
Steve Lacey <steve@stevelacey.net>
2011-02-19 19:01:29 +0000

    Imported data from UWE Google Map, pushed all data out to json.

--- web/js/main.js
+++ web/js/main.js
@@ -30,34 +30,30 @@ $('.map').live("pagecreate", function() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
+  $.each(['uwe', 'blocks'], function(i, file) {
+    $.ajax({
+      url: 'data/' + file + '.json',
+      dataType: 'json',
+      success: function(data) {
+        $.each(data.poi, function(key, poi) {
+          paths = [];
 
-  var paths = {
-    q: [
-      new google.maps.LatLng(51.50172,-2.548866),
-      new google.maps.LatLng(51.501764,-2.548498),
-      new google.maps.LatLng(51.501485,-2.548415),
-      new google.maps.LatLng(51.501465,-2.548584),
-      new google.maps.LatLng(51.501433,-2.548576),
-      new google.maps.LatLng(51.501468,-2.548233),
-      new google.maps.LatLng(51.500921,-2.548082),
-      new google.maps.LatLng(51.500881,-2.548442),
-      new google.maps.LatLng(51.5013,-2.548565),
-      new google.maps.LatLng(51.501295,-2.548624),
-      new google.maps.LatLng(51.501221,-2.548608),
-      new google.maps.LatLng(51.5012,-2.548799),
-      new google.maps.LatLng(51.501402,-2.548858),
-      new google.maps.LatLng(51.501408,-2.548772)
-    ]
-  };
+          $.each(poi.polygon, function(i, point) {
+            paths[i] = new google.maps.LatLng(point.lat, point.lng);
+          });
 
-  var polygon = new google.maps.Polygon({
-    paths: paths.q,
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35
+          var polygon = new google.maps.Polygon({
+            paths: paths,
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35
+          });
+
+          polygon.setMap(map);
+        });
+      }
+    });
   });
-
-  polygon.setMap(map);
 });
