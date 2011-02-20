354855129c8b93857e14e4d9e7a572de7e79bafc
Steve Lacey <steve@stevelacey.net>
2011-02-20 00:08:10 +0000

    Corrected layout indenting.

--- modules/find.php
+++ modules/find.php
@@ -1,12 +1,12 @@
-  <div data-role="header">
-    <h1>Points of Interest</h1>
-  </div>
+    <div data-role="header">
+      <h1>Points of Interest</h1>
+    </div>
 
-  <div data-role="content">
-    <ul data-role="listview">
-      <li><a href="/find/coffee">Coffee</a></li>
-      <li><a href="/find/bar">Bar</a></li>
-      <li><a href="/find/food">Food</a></li>
-      <li><a href="/find/supermarket">Supermarket</a></li>
-    </ul>
-  </div>
+    <div data-role="content">
+      <ul data-role="listview">
+        <li><a href="/find/coffee">Coffee</a></li>
+        <li><a href="/find/bar">Bar</a></li>
+        <li><a href="/find/food">Food</a></li>
+        <li><a href="/find/supermarket">Supermarket</a></li>
+      </ul>
+    </div>
--- modules/map.php
+++ modules/map.php
@@ -1,5 +1,5 @@
-  <div data-role="content">
-    <div class="canvas">
-      <!-- map loads here... -->
+    <div data-role="content">
+      <div class="canvas">
+        <!-- map loads here... -->
+      </div>
     </div>
-  </div>
