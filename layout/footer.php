b1d783d1bc5da08deec7295fbd43a8a9a6ad90ef
Steve Lacey <steve@stevelacey.net>
2011-02-20 00:05:14 +0000

    Corrected layout indenting.

--- layout/footer.php
+++ layout/footer.php
@@ -1,5 +1,5 @@
   <div data-id="footer" data-role="footer" data-position="fixed">
-  	<div data-role="navbar">
+    <div data-role="navbar">
       <ul>
         <li><a href="/" data-role="button" data-icon="home" data-ajax="false">Map</a></li>
         <li><a href="/find" data-role="button" data-icon="search">Find</a></li>
