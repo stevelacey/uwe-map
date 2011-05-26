430145c6778ded02748bdeddb3d8acfad6bb9bbd
Steve Lacey <steve@stevelacey.net>
2011-05-26 16:10:37 +0100

    Further updates to find menu, added block icon.

--- modules/find.php
+++ modules/find.php
@@ -1,14 +1,17 @@
         <panel id="find" title="Find" icon="42-photos.png">
-          <page id="carousel_page" title="Carousel">
+          <page id="list" title="Find">
             <div data-role="header">
               <h1>Points of Interest</h1>
             </div>
 
             <nav data-role="content">
               <ul data-role="listview">
+                <li class="arrow"><a href="#bank" class="bank">Bank</a></li>
                 <li class="arrow"><a href="#bars" class="bar">Bars</a></li>
+                <li class="arrow"><a href="#blocks" class="block">Blocks</a></li>
                 <li class="arrow"><a href="#cafes" class="cafe">Caf&eacute;s</a></li>
-                <li class="arrow"><a href="#cash" class="cash">Cash Points</a></li>
+                <li class="arrow"><a href="#car-parks" class="car-park">Car Parks</a></li>
+                <li class="arrow"><a href="#cash" class="cash">Cash Point</a></li>
                 <li class="arrow"><a href="#coffee" class="coffee">Coffee</a></li>
                 <li class="arrow"><a href="#supermarkets" class="supermarket">Supermarkets</a></li>
               </ul>
